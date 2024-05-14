ac5bd81656e4296558197d15c0bb5b8e075b70af
Kade Robertson <kade@kaderobertson.dev>
2024-05-14 15:58:47 -0400

    test: use later wasm-bindgen

--- tests/all/utils/fixture.rs
+++ tests/all/utils/fixture.rs
@@ -144,7 +144,7 @@ impl Fixture {
                     # bindgen downloaded is what we expect, and if `=` is
                     # removed then it will download whatever the newest version
                     # of wasm-bindgen is which may not be what's listed here.
-                    wasm-bindgen = "=0.2.74"
+                    wasm-bindgen = "=0.2.92"
 
                     [dev-dependencies]
                     wasm-bindgen-test = "0.3"
@@ -222,7 +222,7 @@ impl Fixture {
 
         static INSTALL_WASM_BINDGEN: Once = Once::new();
         let cache = self.cache();
-        let version = "0.2.74";
+        let version = "0.2.92";
 
         let download = || {
             if let Ok(download) =
